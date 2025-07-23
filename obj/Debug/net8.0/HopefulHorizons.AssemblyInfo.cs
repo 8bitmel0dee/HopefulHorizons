d053ae858cf1f7100a62e1047a9dea092ee2a56b
Melody <8bitmel0dee@gmail.com>
2025-07-23 19:43:17 -0400

    refactor: polish homepage UI and improve mobile responsiveness

--- obj/Debug/net8.0/HopefulHorizons.AssemblyInfo.cs
+++ obj/Debug/net8.0/HopefulHorizons.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HopefulHorizons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205608b8c4f5b689c3377961f061cd6682f8b2a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0e7a20d32edda42b25b107b4f19a2542496116")]
 [assembly: System.Reflection.AssemblyProductAttribute("HopefulHorizons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HopefulHorizons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
