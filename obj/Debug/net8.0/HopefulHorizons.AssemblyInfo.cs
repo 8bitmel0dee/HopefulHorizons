ec394059a3208e82745b974fb4654cdf1388d14d
Melody <8bitmel0dee@gmail.com>
2026-01-17 16:14:24 -0500

    style cleanup

--- obj/Debug/net8.0/HopefulHorizons.AssemblyInfo.cs
+++ obj/Debug/net8.0/HopefulHorizons.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HopefulHorizons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f53090253b1b92e72223adab5032ebeb60bbec6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83de93f1afe62d9646e3b205dcc0b3ada97a8b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HopefulHorizons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HopefulHorizons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
